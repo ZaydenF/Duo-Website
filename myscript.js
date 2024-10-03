598a3d5523e93776f059933ae92f3ab2dd1d28cc
ZaydenF <zaydenfleming@gmail.com>
2024-10-03 11:32:33 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+  $('.radioform').submit(function(event) {
+        event.preventDefault(); // Prevent form from refreshing the page
+
+        // Get the selected radio button
+        var selectedOption = $('input[name="geography"]:checked').val();
+
+        // Check if any radio button was selected
+        if (selectedOption === undefined) {
+            alert("Please select an option before submitting.");
+            return;
+        }
+
+        // Show the appropriate alert based on the selected option
+        if (selectedOption === 'wrong') {
+            $('#dangerAlert').show();
+            $('#successAlert').hide();
+        } else if (selectedOption === 'correct') {
+            $('#successAlert').show();
+            $('#dangerAlert').hide();
+        
+	
   $(".answer").hide();
   $(".showA").click(function() {
 	$(this).next(".answer").slideToggle();
